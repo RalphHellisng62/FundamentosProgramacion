0d5a9ff9a634217422f2b3a2e0ee9d90766aec88
Rafael Arenas Rivera Arenas <arenasrafael889@gmail.com>
2024-10-11 12:26:07 -0600

    ejerccio1

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+'''
+Ejercicio 1: 
+Escribir un programa que pregunte al usuario su nombre 
+Entradas:
+nombre: string
+Salida:
+saludo: string
+
+'''
+nombre = input('escribe tu nombre: ')
+saludo = "hola" + nombre + "!"
+print(saludo)
+
